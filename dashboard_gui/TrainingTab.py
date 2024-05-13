57c7b15a207912a8affe501c46d27f936a8dba66
sirus20x6 <sirus20x6@gmail.com>
2024-05-12 19:23:49 -0500

    coming along nicely

--- dashboard_gui/TrainingTab.py
+++ dashboard_gui/TrainingTab.py
@@ -7,6 +7,9 @@ from .ModelConfigurationTab import ModelConfigurationTab
 from .OptimizationTab import OptimizationTab
 from .PipelinesTab import PipelinesTab
 from .TrainerTab import TrainerTab
+from .tab_data_management import DataManagementTab
+from .tab_model_info import ModelInfoTab
+from .tab_graphs import GraphsTab
 
 class TrainingTab(QWidget):
     def __init__(self):
@@ -17,12 +20,15 @@ class TrainingTab(QWidget):
         layout = QVBoxLayout()
         tab_widget = QTabWidget()
 
+
+        tab_widget.addTab(DataManagementTab(), "DataManagement")
         tab_widget.addTab(ConfigurationTab(), "Configuration")
         tab_widget.addTab(LoggingTab(), "Logging")
+        tab_widget.addTab(ModelInfoTab(), "Model Info")
         tab_widget.addTab(ModelConfigurationTab(), "Models")
         tab_widget.addTab(OptimizationTab(), "Optimization")
         tab_widget.addTab(PipelinesTab(), "Pipelines")
         tab_widget.addTab(TrainerTab(), "Trainer")
-
+        tab_widget.addTab(GraphsTab(), "Graphs")
         layout.addWidget(tab_widget)
         self.setLayout(layout)
--- dashboard_gui/main.py
+++ dashboard_gui/main.py
@@ -8,25 +8,69 @@ class MainApp(QMainWindow):
         self.title = 'Transformers Training Dashboard'
         self.left = 100
         self.top = 100
-        self.width = 800
-        self.height = 1200
+        self.width = 900
+        self.height = 600  # Adjusted for better proportionality
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.apply_stylesheet()
 
         training_tab = TrainingTab()
         self.setCentralWidget(training_tab)
         self.show()
 
+    def apply_stylesheet(self):
+        self.setStyleSheet("""
+            QMainWindow {
+                background-color: #f0f0f0;
+            }
+            QPushButton {
+                background-color: #005599;
+                color: white;
+                border: none;
+                padding: 10px;
+                border-radius: 5px;
+                font-size: 16px;
+            }
+            QPushButton:hover {
+                background-color: #006699;
+            }
+            QLabel, QLineEdit, QComboBox {
+                font-size: 16px;
+                padding: 2px;
+                margin: 4px;
+            }
+            QLineEdit {
+                border: 1px solid #ccc;
+                border-radius: 4px;
+            }
+            QComboBox {
+                border: 1px solid #aaa;
+                border-radius: 4px;
+                padding: 1px 18px 1px 3px;  /* Room for the dropdown arrow */
+                min-width: 6em;
+            }
+            QComboBox::drop-down {
+                subcontrol-origin: padding;
+                subcontrol-position: top right;
+                width: 15px;
+                border-left-width: 1px;
+                border-left-color: darkgray;
+                border-left-style: solid; /* just a single line */
+                border-top-right-radius: 3px; /* same radius as the QComboBox */
+                border-bottom-right-radius: 3px;
+            }
+            QComboBox::down-arrow {
+                image: url(/path/to/your/image.png); /* Add your path */
+            }
+        """)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # Optional: Apply a dark theme or other QSS here globally
+    # import qdarkstyle
+    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     ex = MainApp()
     sys.exit(app.exec_())
-    
-    
-#        tab_widget.addTab(ModelInfoTab(), "Model Information")
-#        tab_widget.addTab(TrainingTab(), "Training Settings")
-#        tab_widget.addTab(DataManagementTab(), "Training Data")
-#        tab_widget.addTab(GraphsTab(), "Graphs")
